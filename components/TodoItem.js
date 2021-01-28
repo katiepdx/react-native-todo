50897194663f6637ac334f54953e245d9b2ccc71
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-27 16:44:03 -0800

    add done and not done images

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import doneIcon from '../assets/completed/done.png'
+import notDoneIcon from '../assets/completed/not-done.png'
 
 export const TodoItem = ({ todo }) => {
   return (
     <View style={styles.todoStyles}>
       <Text style={styles.text}>Todo: {todo.todo}</Text>
       <Text style={styles.text, todo.completed ? styles.done : styles.notDone}>
-        Status: {todo.completed ? 'DONE' : 'not done'}
+        Status:
+        {todo.completed
+          ? 'DONE' && <Image source={doneIcon} style={styles.image} />
+          : 'not done' && <Image source={notDoneIcon} style={styles.image} />}
       </Text>
+
       <Text>Notes: {todo.notes}</Text>
     </View>
   )
@@ -28,6 +34,10 @@ const styles = StyleSheet.create({
   },
   notDone: {
     backgroundColor: 'red'
+  },
+  image: {
+    height: 20,
+    width: 20
   }
 
 });
